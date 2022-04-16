97af4811d434a68c78b6b12452b523c8b5c0a378
Fatykhov <fatykhov@pm.me>
2022-04-16 18:37:42 +0200

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -6,38 +6,37 @@
 #include "disk/disk.h"
 
 int main() {
-
     std::string command;
     int digits;
-
     std::cout << "PC simulator" << std::endl;
-
         do {
             std::cout << "Input command: ";
             std::cin >> command;
-
-            if (command == "kbd") {
-                inputDigits(digits);
-            } else if (command == "gpu") {
-                outputDigits(digits);
-            } else if (command == "cpu") {
-                sum(digits);
-            } else if (command == "ram") {
-                std::string ramCommand;
-                std::cout << "Input command for ram: ";
-                std::cin >> ramCommand;
-                    if (ramCommand == "read") {
-                        read(digits);
-                    } else if (ramCommand == "write") {
-                        write(digits);
-                    }
-            } else if (command == "disk") {
-                std::string diskCommand;
-                std::cout << "Input command for disk: ";
-                std::cin >> diskCommand;
-            }
-
+                if (command == "kbd") {
+                    inputDigits(digits);
+                } else if (command == "gpu") {
+                    outputDigits(digits);
+                } else if (command == "cpu") {
+                    sum(digits);
+                } else if (command == "ram") {
+                    std::string ramCommand;
+                    std::cout << "Input command for ram: ";
+                    std::cin >> ramCommand;
+                        if (ramCommand == "read") {
+                            read(digits);
+                        } else if (ramCommand == "write") {
+                            write(digits);
+                        }
+                } else if (command == "disk") {
+                    std::string diskCommand;
+                    std::cout << "Input command for disk: ";
+                    std::cin >> diskCommand;
+                        if (diskCommand == "save") {
+                            save(digits);
+                        } else if (diskCommand == "load") {
+                            load(digits);
+                        }
+                }
         } while (command != "exit");
-
     return 0;
 }
